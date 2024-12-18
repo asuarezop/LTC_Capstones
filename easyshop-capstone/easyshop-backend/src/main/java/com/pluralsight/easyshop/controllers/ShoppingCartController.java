1a64fb701962581f9fe8355c43d67ab24973e124
Anthony Suarez <asuarezop@gmail.com>
2024-12-17 20:48:15 -0500

    Removed UserDAO sql strings and added in POST method to add a product to shopping cart in ShoppingCartController

--- easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/controllers/ShoppingCartController.java
+++ easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/controllers/ShoppingCartController.java
@@ -1,8 +1,8 @@
 package com.pluralsight.easyshop.controllers;
 
+import com.pluralsight.easyshop.models.Product;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import com.pluralsight.easyshop.data.ProductDao;
 import com.pluralsight.easyshop.data.ShoppingCartDao;
@@ -16,20 +16,22 @@ import java.security.Principal;
 // only logged in users should have access to these actions
 @RestController
 @CrossOrigin
-public class ShoppingCartController
-{
+@RequestMapping(path = "/cart")
+public class ShoppingCartController {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
 
-
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
+    public ShoppingCart getCart(Principal principal) {
+        try {
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
@@ -38,15 +40,19 @@ public class ShoppingCartController
 
             // use the shoppingcartDao to get all items in the cart and return the cart
             return null;
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping(path = "/products/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ShoppingCart addProductToCart(@PathVariable int id, @RequestBody Product product) {
+//        shoppingCartDao
+        return new ShoppingCart();
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
--- easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/data/mysql/MySqlUserDao.java
+++ easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/data/mysql/MySqlUserDao.java
@@ -46,10 +46,10 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao {
     public List<User> getAll() {
         List<User> users = new ArrayList<>();
 
-        String sql = "SELECT * FROM users";
         try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(sql);
-
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT * FROM users
+                    """);
             ResultSet row = statement.executeQuery();
 
             while (row.next()) {
@@ -65,9 +65,11 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao {
 
     @Override
     public User getUserById(int id) {
-        String sql = "SELECT * FROM users WHERE user_id = ?";
         try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT * FROM users
+                    WHERE user_id = ?
+                    """);
             statement.setInt(1, id);
 
             ResultSet row = statement.executeQuery();
@@ -84,22 +86,20 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao {
 
     @Override
     public User getByUserName(String username) {
-        String sql = "SELECT * " +
-                " FROM users " +
-                " WHERE username = ?";
-
         try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT * FROM users
+                    WHERE username = ?
+                    """);
             statement.setString(1, username);
 
             ResultSet row = statement.executeQuery();
             if (row.next()) {
-
                 User user = mapRow(row);
                 return user;
             }
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
 
         return null;
