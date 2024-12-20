648c99e44eee6f45ebc11c77da13a186cc9810e1
Anthony Suarez <asuarezop@gmail.com>
2024-12-20 01:52:51 -0500

    Added README file

---
+++ easyshop-capstone/easyshop-backend/src/README.md
@@ -0,0 +1,59 @@
+![easyshop_header](main/resources/assets/easyshop_console_header.png)
+
+### A client/web server application powered by Spring Boot intended to support e-commerce websites.
+
+## Starting the application
+
+> Note: You will need access to MySQL WorkBench alongside Postman to run integration tests associated 
+>       with the application. 
+
+Navigating to the easyshop-capstone directory, you will find three Postman collections to run
+When running these tests, run the entire collection as a whole as opposed to individually.
+- This avoids any potential errors being thrown inside of Postman
+
+Demo of running a collection inside Postman:
+
+![easyshop_running_collection](main/resources/assets/easyshop_running_collections.gif)
+
+## Troubleshooting
+
+If there is an issue with a test failing, follow these steps:
+
+1) Reset the database (execute the create_database sql file inside database directory)
+2) Rerun the application (this clears any request messages leftover from previous test runs)
+3) Rerun the collection (using the same method as above demonstration)
+
+> For debugging purposes, check the message returned in the body of request. 
+> You'll get a more detailed explanation behind the error.
+
+## Frontend Page
+
+![easyshop_frontend](main/resources/assets/easyshop_frontend_filter.gif)
+
+## Bug Fixes
+- Home Screen showed minimum price field duplicated
+- Login not working
+
+## Backend Page
+![easyshop_backend](main/resources/assets/easyshop_backend_server.gif)
+
+## Bug Fixes
+- POST request mapping inside ProductController was using incorrect method
+- Search functionality not implemented correctly, fixed path for request in Postman
+- Duplicate products showing up in the database, added logic to check for existing products by name before inserting a new product with similar properties
+
+## Work Flow
+
+Driven by test implementation and following logical sequence of HTTP request ordering in easyshop collections.
+Starting from categories and working my way through the rest of the controller classes to add the missing logic for each request type. 
+- GET -> POST -> PUT -> DELETE
+
+For testing, I ran each request individually in Postman first before running it again in the entire collection 
+just to make sure the logic was correct. 
+
+> Some slight modifications were made inside the collection variables for each collection to prevent
+> overwriting of values when running the next collection.
+
+### Author
+Anthony Suarez - https://github.com/asuarezop
+
--- easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/data/mysql/MySqlOrderDao.java
+++ easyshop-capstone/easyshop-backend/src/main/java/com/pluralsight/easyshop/data/mysql/MySqlOrderDao.java
@@ -3,8 +3,6 @@ package com.pluralsight.easyshop.data.mysql;
 import JavaHelpers.ColorCodes;
 import com.pluralsight.easyshop.data.OrderDao;
 import com.pluralsight.easyshop.models.*;
-import org.apache.tomcat.jni.Time;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -12,7 +10,6 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,58 +18,6 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
     public MySqlOrderDao(DataSource dataSource) {
         super(dataSource);
     }
-
-//    @Override
-//    public Order getOrderById(int userId) {
-//        String queryOne = """
-//                SELECT * FROM orders
-//                JOIN order_line_items ON orders.order_id = order_line_items.order_id
-//                WHERE user_id = ?
-//                """;
-//
-//        try (Connection conn = getConnection()) {
-//            PreparedStatement statement = conn.prepareStatement("""
-//                    SELECT orders.order_id, user_id, date, address, city, state, zip, shipping_amount, order_line_items.product_id, products.name, order_line_items.sales_price, order_line_items.quantity, order_line_items.discount
-//                    FROM orders
-//                    JOIN order_line_items ON orders.order_id = order_line_items.order_id
-//                    JOIN products ON products.product_id = order_line_items.product_id
-//                    WHERE user_id = ?;
-//                    """);
-//            statement.setInt(1, userId);
-//
-//            ResultSet rs = statement.executeQuery();
-//
-//            while (rs.next()) {
-//                //Create an Order object
-//                String address = rs.getString("address");
-//                String city = rs.getString("city");
-//                String state = rs.getString("state");
-//                String zip = rs.getString("zip");
-//                BigDecimal shippingAmt = rs.getBigDecimal("shipping_amount");
-////                Order userOrder = new Order(userId, LocalDateTime.now(), address, city, state, zip, shippingAmt);
-//
-//
-//
-//
-//                for (ShoppingCartItem item: userCart.getItems().values()) {
-//                    //Create order line items
-//                    int productId = rs.getInt("order_line_items.product_id");
-//                    String name = rs.getString("products.name");
-//                    double salesPrice = rs.getDouble("order_line_items.sales_price");
-//                    int quantity = rs.getInt("order_line_items.quantity");
-//                    BigDecimal discount = rs.getBigDecimal("order_line_items.discount");
-//
-////                    OrderLineItem lineItem = new OrderLineItem(productId, name, salesPrice, quantity, discount);
-//
-//
-//                }
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        return new Order();
-//    }
 
     @Override
     public Order createOrder(int userId, ShoppingCart cart, Profile userProfile) {
@@ -104,9 +49,10 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
                 //Create the order object associated to a user
                 fullOrder = new Order(orderId, userId);
 
-                //Adding products to the order (2nd step)
+                //Part 2 - Adding products to the order
                 List<OrderLineItem> lineItems = convertCartToLineItems(fullOrder, cart);
 
+                //Adding additional user background info
                 String userAddress = userProfile.getAddress();
                 String userCity = userProfile.getCity();
                 String userState = userProfile.getState();
@@ -123,18 +69,7 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        /* Requirements:
-         *  1) Retrieve the current user's shopping cart
-         *  2) Create a new Order
-         *  3) Create a new OrderLineItem for each shopping cart item
-         *  4) Add OrderLineItems to the db
-         *
-         * //What's being returned in an order
-         *   - OrderLineItems
-         *   - User background info (address, city, state, zip) --> can be gotten from user_id joining on users table
-         * */
 
-        //Last step
         return null;
     }
 
